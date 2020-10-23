946feee4a676f634c9a4609a57474a8e23745e1a
Julio Quintana <julioeq29@gmail.com>
2020-10-23 22:51:28 +0100

    strava picture url to navbar and placeholder when empty

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:strava]
 
+  has_one_attached :photo
+
   def self.create_from_provider_data(provider_data)
+    # raise
     email = provider_data.info.name.split(" ").join.downcase + provider_data.uid + "@figz.com"
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = email
       user.password = Devise.friendly_token[0, 20]
+      user.strava_picture_url = provider_data.extra.raw_info.profile
     end
   end
 end
