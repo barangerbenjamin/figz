d922e9c835e712a979dcf7a16a6a09fd8d03be2a
Julio Quintana <julioeq29@gmail.com>
2020-10-23 23:09:26 +0100

    adapted strava oauth for updating if user exists and we dont have all fields filled out

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,9 +1,9 @@
 class OmniauthController < Devise::OmniauthCallbacksController
 
   def strava
-      @user = User.create_from_provider_data(request.env['omniauth.auth'])
-      if @user.persisted?
-          sign_in_and_redirect @user
+      user = User.create_from_provider_data(request.env['omniauth.auth'])
+      if user.persisted?
+          sign_in_and_redirect user
           set_flash_message(:notice, :success, kind: 'Strava') if is_navigational_format?
       else
           flash[:error] = 'There was a problem signing you in through Strava'
