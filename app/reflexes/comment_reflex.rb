50ff16b88a30dc24f8f0405731d17e1c514ecb73
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-10-25 19:18:06 +0100

    destroy comment with stimulus reflex

--- app/reflexes/comment_reflex.rb
+++ app/reflexes/comment_reflex.rb
@@ -1,8 +1,13 @@
 class CommentReflex < ApplicationReflex
   def create
-    comment = Comment.new(content: params[:comment][:content])
-    comment.user = User.find(element.dataset.user)
-    comment.route = Route.find(params[:id])
-    comment.save
+    @comment = Comment.new(content: params[:comment][:content])
+    @comment.user = User.find(element.dataset.user)
+    @comment.route = Route.find(params[:id])
+    @comment.save if @comment.valid?
+  end
+
+  def destroy
+    @comment = Comment.find(element.dataset.comment_id)
+    @comment.destroy
   end
 end
