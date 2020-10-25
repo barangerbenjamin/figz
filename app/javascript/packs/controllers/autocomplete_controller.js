cf27b28ee9e095316fb28bca6c180dc05ea93aac
Benjamin Baranger <baranger.benjamin@gmail.com>
2020-10-25 13:31:19 +0100

    remove useless listener + add missing ;

--- app/javascript/packs/controllers/autocomplete_controller.js
+++ app/javascript/packs/controllers/autocomplete_controller.js
@@ -1,22 +1,20 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "start", "end" ]
+  static targets = [ "start", "end" ];
 
   connect() {
-    document.addEventListener('DOMContentLoaded', () => {
-      let autocompletes = [this.startTarget, this.endTarget];
-      
-      function initAutocomplete() {
-        autocompletes.forEach((autocomplete) => {
-          autocomplete = new google.maps.places.Autocomplete(
-            autocomplete,
-            { types: ["geocode"] }
-          );
-          autocomplete.setFields(["address_component"]);
-        });
-      }
-      initAutocomplete()
-    })
+    let autocompletes = [this.startTarget, this.endTarget];
+    
+    function initAutocomplete() {
+      autocompletes.forEach((autocomplete) => {
+        autocomplete = new google.maps.places.Autocomplete(
+          autocomplete,
+          { types: ["geocode"] }
+        );
+        autocomplete.setFields(["address_component"]);
+      });
+    }
+    initAutocomplete();
   }
 }
