7aba59ae4bebc06272025a7a26985ba3ae32caac
Julio Quintana <julioeq29@gmail.com>
2020-10-27 16:45:44 +0000

    some front eeeeend for the routes index

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,30 @@
 class CommentsController < ApplicationController
-    before_action :set_comment, only: [:update, :destroy]
+  before_action :set_comment, only: [:update, :destroy]
 
-    def index
-        @comments = comment.all
-    end
+  def index
+    @comments = comment.all
+  end
 
-    def update
-        if @comment.update(comment_params)
-            redirect_to route_path(@comment.route), notice: "comment updated"
-        else
-            render :edit
-        end
+  def update
+    if @comment.update(comment_params)
+      redirect_to route_path(@comment.route), notice: "comment updated"
+    else
+      render :edit
     end
+  end
 
-    def destroy
-        @comment.destroy
-        redirect_to route_path(@comment.route), notice: "comment destroyed"
-    end
+  def destroy
+    @comment.destroy
+    redirect_to route_path(@comment.route), notice: "comment destroyed"
+  end
 
-    private
+  private
 
-    def set_comment
-        @comment = Comment.find(params[:id])
-    end
-    
-    def comment_params
-        params.require(:comment).permit(:content)
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
